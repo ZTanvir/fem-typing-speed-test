23352e5aee96f38c99da0218af0e0c6c53519355
zahirul <zahirulopel@gmail.com>
2026-01-10 10:07:29 +0600

    add Logo component that will display logo based on media screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1 className="font-sora font-800">Font size</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import logoLarge from "../assets/images/logo-large.svg";
+import logoSmall from "../assets/images/logo-small.svg";
+
+const Logo = () => {
+  return (
+    <picture className="bg-amber-950">
+      <source media="(min-width:425px)" srcSet={logoLarge} />
+      <img src={logoSmall} alt="logo" />
+    </picture>
+  );
+};
+export default Logo;
