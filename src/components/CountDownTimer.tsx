f0e28b054fcb72c54dc21036f9971ea3d4878213
zahirul <zahirulopel@gmail.com>
2026-01-12 11:41:46 +0600

    fixed time updated issue

--- src/components/CountDownTimer.tsx
+++ src/components/CountDownTimer.tsx
@@ -5,30 +5,55 @@ type CountDownTimerProps = {
   isTestRunning: boolean;
 };
 
-const CountDownTimer = ({
-  mode,
-  isTestRunning,
-  setTestRunning,
-}: CountDownTimerProps) => {
+const CountDownTimer = ({ mode, isTestRunning }: CountDownTimerProps) => {
   const [second, setSeconds] = useState(mode === "timed" ? 60 : 0);
+
+  const formatTime = () => {
+    // console.log("I am in format time");
+    if (mode === "timed") {
+      return `0:${second}`;
+    } else if (mode === "passage") {
+      if (mode === "passage") {
+        const minute = Math.trunc(second / 60);
+        const sec = second % 60;
+        if (sec >= 0 && sec <= 9) {
+          return `${minute}:0${sec}`;
+        }
+        return `${minute}:${sec}`;
+      }
+    }
+  };
 
   useEffect(() => {
     let intervalId: number;
     function startTimer() {
       if (isTestRunning) {
         intervalId = setInterval(() => {
-          if (mode === "timed") {
-            setSeconds(second - 1);
-          } else if (mode === "passage") {
-            setSeconds(second + 1);
-          }
+          setSeconds((prevSecond) => {
+            if (mode === "timed") {
+              if (second <= 0) {
+                clearInterval(intervalId);
+                return 0;
+              }
+              return prevSecond - 1;
+            } else {
+              return prevSecond + 1;
+            }
+          });
         }, 1000);
       }
     }
 
     startTimer();
-  }, [isTestRunning]);
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, [isTestRunning, mode]);
 
-  return <div></div>;
+  return (
+    <div>
+      Time: <span>{formatTime()}</span>
+    </div>
+  );
 };
 export default CountDownTimer;
