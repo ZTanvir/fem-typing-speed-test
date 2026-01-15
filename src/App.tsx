76ae4de3ca7394716b897a9a6cef9b5a9b0fb053
zahirul <zahirulopel@gmail.com>
2026-01-15 10:31:19 +0600

    add Test screen that display typing question based on test started or not

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import "./App.css";
+import TestScreen from "./components/TestScreen";
 
 function App() {
-  return <></>;
+  const [isTestRunning, setIsTestRunning] = useState(false);
+  return (
+    <div className="bg-neutral-900">
+      <TestScreen
+        isTestRunning={isTestRunning}
+        setIsTestRunning={setIsTestRunning}
+        question="The sun rose over the quiet town. Birds sang in the trees as people woke up and started their day. It was going to be a warm and sunny morning."
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TestScreen.tsx
@@ -0,0 +1,43 @@
+import IconRestart from "../assets/images/icon-restart.svg";
+
+type TestScreenProps = {
+  question: string;
+  isTestRunning: boolean;
+  setIsTestRunning: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const TestScreen = ({
+  question,
+  isTestRunning,
+  setIsTestRunning,
+}: TestScreenProps) => {
+  return (
+    <div className="relative">
+      {!isTestRunning && (
+        <div className="absolute z-1 flex h-full w-full flex-col items-center justify-center gap-1 backdrop-blur-[5px]">
+          <button
+            onClick={() => setIsTestRunning(true)}
+            className="rounded-lg bg-blue-600 px-4 py-2 font-bold text-neutral-50 hover:cursor-pointer"
+          >
+            Start Typing Test
+          </button>
+          <p className="text-center text-sm text-neutral-50 sm:text-base">
+            Or click the text and start typing
+          </p>
+        </div>
+      )}
+
+      <div className="py-4 text-2xl sm:py-6 sm:text-4xl">
+        {question}
+
+        <hr className="my-4 text-neutral-700 opacity-40 sm:my-6" />
+        {isTestRunning && (
+          <button className="mx-auto flex items-center gap-2 rounded-lg bg-neutral-700 px-3 py-2 text-lg text-white hover:cursor-pointer">
+            <span>Restart Test </span>
+            <img className="w-4" src={IconRestart} alt="Restart test" />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+export default TestScreen;
