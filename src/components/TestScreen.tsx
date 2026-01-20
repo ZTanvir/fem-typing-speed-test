90948fd27708b2d2f3b60adfefda90b5bdf56bc0
zahirul <zahirulopel@gmail.com>
2026-01-20 10:09:42 +0600

    get user typed keybord key

--- src/components/TestScreen.tsx
+++ src/components/TestScreen.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import IconRestart from "../assets/images/icon-restart.svg";
 
 type TestScreenProps = {
@@ -10,7 +11,19 @@ const TestScreen = ({
   isTestRunning,
   setIsTestRunning,
 }: TestScreenProps) => {
+  const questionEl = useRef<HTMLDivElement>(null);
   console.log(question.split(""));
+
+  const handleUserKeyPress = (e: React.KeyboardEvent) => {
+    console.log(e.key);
+  };
+
+  useEffect(() => {
+    if (isTestRunning && questionEl.current) {
+      questionEl.current.focus();
+    }
+  }, [isTestRunning]);
+
   return (
     <div className="relative">
       {!isTestRunning && (
@@ -27,16 +40,21 @@ const TestScreen = ({
         </div>
       )}
 
-      <div className="py-4 text-2xl sm:py-6 sm:text-4xl">
+      <div
+        onKeyDown={handleUserKeyPress}
+        ref={questionEl}
+        tabIndex={1}
+        className="py-4 text-2xl sm:py-6 sm:text-4xl"
+      >
         {question}
 
         <hr className="my-4 text-neutral-700 opacity-40 sm:my-6" />
-        <button
-          className={`${isTestRunning ? "visible" : "invisible"} mx-auto flex items-center gap-2 rounded-lg bg-neutral-700 px-3 py-2 text-lg text-white hover:cursor-pointer`}
-        >
-          <span>Restart Test </span>
-          <img className="w-4" src={IconRestart} alt="Restart test" />
-        </button>
+        {isTestRunning && (
+          <button className="mx-auto flex items-center gap-2 rounded-lg bg-neutral-700 px-3 py-2 text-lg text-white hover:cursor-pointer">
+            <span>Restart Test </span>
+            <img className="w-4" src={IconRestart} alt="Restart test" />
+          </button>
+        )}
       </div>
     </div>
   );
