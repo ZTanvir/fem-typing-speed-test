8d33172079977adda02beef72e5ec16f08d3459f
zahirul <zahirulopel@gmail.com>
2026-02-01 09:41:41 +0600

    fixed shuffle data aguments

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -26,9 +26,7 @@ const HomePage = () => {
         let data = await response.json();
         // in server /data/db.json/difficultyType not accessible
         data = import.meta.env.DEV ? data : data[difficultyType];
-        const shuffledData: Question[] = helperFunctions.shuffle(
-          data[difficultyType],
-        );
+        const shuffledData: Question[] = helperFunctions.shuffle(data);
         if (shuffledData.length > 0) {
           setQuestion(shuffledData[0].text);
         }
